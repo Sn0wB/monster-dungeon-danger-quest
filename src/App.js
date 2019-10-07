8c946c6a1e24cd472ec787d90660da6ae055dbf4
Sn0wB <pana.sonia@hotmail.com>
2019-10-07 22:21:14 +0200

    Removed zombie code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import Game from './Game/Game'
 import './App.css';
 
@@ -7,17 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-        </header>
         <body className="App-body">
           <Game />
         </body>
